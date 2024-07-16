39df49b2eeae0fe8e159b59bc95ff32ed558a625
oragegu <me@dansmonorage.blue>
2024-07-16 15:25:31 +0200

    buttons in the modal popup

--- src/app/share-buttons/share-buttons.component.ts
+++ src/app/share-buttons/share-buttons.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-share-buttons',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './share-buttons.component.html',
   styleUrl: './share-buttons.component.scss'
 })
