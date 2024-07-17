58fc0f1adaa12bc18b3407b8920135a3317d1b42
oragegu <me@dansmonorage.blue>
2024-07-17 23:52:05 +0200

    main button hovered effect good good

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts,scss}"
+    "./src/**/*.{html,ts,scss}",
+    "./src/*.{html,ts, scss}"
   ],
   theme: {
     colors: {
